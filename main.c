7b23b2163b139adbebcd2ab7bfb51a0627bdbfdc
olllxa <oklashtorn@gmail.com>
2025-05-21 20:52:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,37 @@
-#include "filesystem.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "filesystem.h"
 
-int main() {
-    // 1. Создаём новый файл
-    const char* filename = "example.txt";
-    const char* content = "This is file content\nLine 2\nLine 3";
+static const char *FS_PATH = "disk.filesystem";
+
+int main(void) {
+    FileSystem fs = {0};
+
+    if (fs_load(FS_PATH, &fs) != 0) {
+        printf("Создаем новую файловую систему\n");
+    }
+
+    const char *filename = "test.txt";
+    const char *content = "Это содержимое тестового файла\nВторая строка";
     
-    create_file(filename, content);
-    printf("Файл '%s' создан\n", filename);
+    if (fs_insert(&fs, filename) == 0) {
+        fs_update(&fs, filename, content);
+        printf("Файл '%s' создан\n", filename);
+    } else {
+        printf("Файл '%s' уже существует\n", filename);
+    }
 
-    // 2. Читаем и выводим содержимое
-    printf("\nСодержимое файла:\n");
-    read_file(filename);
+    char *file_content = NULL;
+    if (fs_select(&fs, filename, &file_content) == 0) {
+        printf("\nСодержимое файла '%s':\n%s\n", filename, file_content);
+        free(file_content);
+    } else {
+        printf("Не удалось прочитать файл '%s'\n", filename);
+    }
 
-    // 3. Удаляем файл (опционально)
-    delete_file(filename);
+    fs_save(FS_PATH, &fs);
+    fs_free(&fs);
     
     return 0;
 }
