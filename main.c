d8c4b88dccfe4a05bd242ed2158c69b96b759275
olllxa <oklashtorn@gmail.com>
2025-05-21 20:43:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 #include "filesystem.h"
+#include <stdio.h>
+
+int main() {
+    // 1. Создаём новый файл
+    const char* filename = "example.txt";
+    const char* content = "This is file content\nLine 2\nLine 3";
+    
+    create_file(filename, content);
+    printf("Файл '%s' создан\n", filename);
+
+    // 2. Читаем и выводим содержимое
+    printf("\nСодержимое файла:\n");
+    read_file(filename);
+
+    // 3. Удаляем файл (опционально)
+    delete_file(filename);
+    
+    return 0;
+}
